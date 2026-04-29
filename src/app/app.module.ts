c819388f835243abe310e6a446d7a29cd5097ccd
Mohamed Saleh <mosalehtag74@icloud.com>
2026-04-29 14:25:28 +0300

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ProductsDetailsComponent } from './components/products-details/products-details.component';
+import { CartComponent } from './components/cart/cart.component';
+import { CategoryComponent } from './components/category/category.component';
+import { BrandsComponent } from './components/brands/brands.component';
+import { OrdersComponent } from './components/orders/orders.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignoutComponent } from './auth/signout/signout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    ProductsComponent,
+    ProductsDetailsComponent,
+    CartComponent,
+    CategoryComponent,
+    BrandsComponent,
+    OrdersComponent,
+    NotFoundComponent,
+    SigninComponent,
+    SignoutComponent
   ],
   imports: [
     BrowserModule,
